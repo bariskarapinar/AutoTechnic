47eb3ed6a03dac422c61b3694fffb754e999f5d5
bariskarapinar <bkarapinar1@hotmail.com>
2026-07-10 13:02:53 +0300

    Color Update

--- app/src/main/java/com/myapp/autotechnic/ui/theme/Color.kt
+++ app/src/main/java/com/myapp/autotechnic/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Neon & Dark Palette
+val NeonBlue = Color(0xFF00F2FF)
+val NeonPurple = Color(0xFFBC13FE)
+val NeonPink = Color(0xFFFF00E5)
+val DarkBackground = Color(0xFF0D0D0D)
+val SurfaceDark = Color(0xFF1A1A1A)
+val GlassWhite = Color(0x1AFFFFFF)
+val GlowBlue = Color(0x3300F2FF)
